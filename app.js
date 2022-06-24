117d6db9125504551ef5a1b5509d9de1a2d7c60c
Kanchana46 <64133928+Kanchana46@users.noreply.github.com>
2022-06-24 15:29:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ app.use(express.urlencoded({ extended: true }));
     res.send("Hello from NodeJS!");
 });*/
 
-
 app.use(express.static(__dirname.replace(/\\/g, "/") + '/dist/frontend/'))
 
 app.get('/', function (req, res) {
